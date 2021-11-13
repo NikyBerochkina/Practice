0cff9f58a0a965f087bc949af61f1e6c05db4a80
Nika Berochkina <berochkina.nika@mail.ru>
2021-11-13 04:38:48 +0300

    make it work

--- prac4_1.c
+++ prac4_1.c
@@ -11,32 +11,49 @@ struct list
 };
 
 // считывание и разделение строк на слова
-int readw(char **w, char *sign, FILE *f_in)
+int readw(char **w, char *sign, FILE *f_in, int *in_k)
 {
     const char *signs = "&|>;<()";
-    int memory = 2, i = 0, c = 0;
-    int in_w = 0; // флаг, показывающий в слове мы сейчас находимся, или нет
-    int in_k = 0; // флаг, показывающий в кавычках мы сейчас, или нет
+    const char *doubsigns = "&|>";
+    int capacity = 2, i = 0, c = 0;
     
-    *w = (char *)malloc(sizeof(char) * memory);
+    
+    *w = (char *)malloc(sizeof(char) * capacity);
 
     if (*sign != 0) //создаем новое слово с специальным символом/символами
     {
         *w = (char *)malloc(2 * sizeof(char));
         (*w)[0] = *sign;
-        
-        if (strchr(signs, c = fgetc(f_in)) != NULL )
+        *sign = 0;
+        c = fgetc(f_in);
+
+        if (strchr(doubsigns, (*w)[0]) != NULL && c == (*w)[0]) 
         {
             (*w)[1] = c;
             *w = (char *)realloc(*w, 3 * sizeof(char));
             (*w)[2] = '\0';
+            c = fgetc(f_in);
+            if (strchr(signs, c))
+            {
+                *sign = c;
+            }
+            else if (c != EOF)
+            {
+                ungetc(c, f_in); // возращаем символ во входной поток
+            }
         } 
         else
         {
             (*w)[1] = '\0';
-            ungetc(c, f_in); // возращаем символ во входной поток
+            if (strchr(signs, c))
+            {
+                *sign = c;
+            }
+            else if (c != EOF)
+            {
+                ungetc(c, f_in); // возращаем символ во входной поток
+            }
         }
-        *sign = 0;
         return 1;
     }
     
@@ -53,37 +70,36 @@ int readw(char **w, char *sign, FILE *f_in)
     {
         if (c == '"') //если встретили кавычку - меняем флаг in_k
         {
-            in_k = (in_k + 1) % 2; // 1 - мы в кавычках, 0 - вне
+            *in_k = !(*in_k); // 1 - мы в кавычках, 0 - вне
         }
-        else if (in_k || (!in_k && (strchr(signs, c) == NULL && !isspace(c))))
+        else if (*in_k || (strchr(signs, c) == NULL && !isspace(c)))
         /* иначе добавляем нынешний символ в слово, если мы находимся в кавычках
            или если мы вне кавычек, но это и не специальный символ, и не пробельный символ */
         {
             (*w)[i++] = c;
-            in_w = 1;
-            if (i == memory) // если надо - расширяем память
+            if (i == capacity) // если надо - расширяем память
             {
-                memory *= 2;
-                *w = (char *)realloc(*w, sizeof(char) * memory);
+                capacity *= 2;
+                *w = (char *)realloc(*w, sizeof(char) * capacity);
             }
-        }
-        else if (c == ' ' || c == '\n' || c == '\t') // не проверяем in_w тк если мы не в слове, мы сюда не попадем
+        } 
+        else if (isspace(c)) 
         {
-            in_w = 0;
             break;
         }
-        else if (strchr(signs, c) != NULL  && !in_w) // если это специальный символ вне слова
+        else if (strchr(signs, c) != NULL ) // если это специальный символ
         {
             *sign = c;
             break;
         }
     }
-    in_w = 0;
+
     if (c == EOF && i == 0)
     {
-        if (in_k)
+        if (*in_k)
         {
             printf("unexpected EOF instead of '\"'\n");
+            exit(EXIT_FAILURE);
         }
         free(*w);
         *w = NULL;
@@ -107,17 +123,19 @@ struct list *addlist(struct list *p, char *w)
     return p;
 }
 
+// печать списка
 void writelist(struct list *p)
 {
     while (p)
     {
-        printf("%s\n", p->word);
+        printf("'%s'\n", p->word);
         p = p->next;
     }
 }
 
 int main(int argc, char **argv)
 {
+    int in_k = 0; // флаг, показывающий в кавычках мы сейчас, или нет
     // 1. определение потока ввода
     FILE* f_in = (argc > 1) ? fopen(argv[1], "r") : stdin;
     if (!f_in)
@@ -129,11 +147,12 @@ int main(int argc, char **argv)
     char sign = 0;
     // 2. считывание слов и добавление их в список
 
-    for (char* w = NULL; readw(&w, &sign, f_in);)
+    for (char* w = NULL; readw(&w, &sign, f_in, &in_k);)
     {
         root = addlist(root, w);
         free(w);
     }
+
     if (argv[1])
     {
         fclose(f_in);
